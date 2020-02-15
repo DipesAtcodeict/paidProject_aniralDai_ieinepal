4b79528a9a1d1493a0844e9fe044f237eb9aeaba
CodeAddictDipesh <45348267+DipesAtcodeict@users.noreply.github.com>
2020-02-15 23:08:33 +0545

    slider added

--- index.js
+++ index.js
@@ -58,3 +58,141 @@ emailForm.addEventListener('submit', e => {
     alert('Please provide all the information correctly');
   }
 });
+
+// caraousel/slider code
+const rightBtnImg = document.querySelector('.rightImg');
+const backBtnImg = document.querySelector('.backImg');
+const captionText = document.querySelector('.caption');
+const carousel = document.querySelector('.carousel');
+const replay = document.querySelector('.replay');
+
+const carouselData = [
+  {
+    imgUrl: 'assets/products/airpollution.jpg',
+    caption:
+      'An air pollution control system for industrial purpose prepared through cross ventilation system helps to save the environment by reducing polluted air which comes out of industry. One of this system was installed for Chaudhary Group.'
+  },
+  {
+    imgUrl: 'assets/products/effluentTreatmentPlant.jpg',
+    caption:
+      'ETP is a process design for treating the industrial waste water for its reuse or safe disposal to the environment.'
+  },
+  {
+    imgUrl: 'assets/products/crushers.jpg',
+    caption: 'Crushers'
+  },
+  {
+    imgUrl: 'assets/products/tram.jpg',
+    caption:
+      'Each tram is custom-made to match the architecture and aesthetics of the property and is equipped with safety systems of the highest quality.'
+  },
+  {
+    imgUrl: 'assets/products/wasteIncinerator.jpg',
+    caption: 'Waste Incinerator.'
+  },
+  {
+    imgUrl: 'assets/products/trust.jpg',
+    caption: 'Metal Frame Truss.'
+  },
+  {
+    imgUrl: 'assets/products/industrialWaterTreatmentPlant.jpg',
+    caption: 'Industrial Water Treatment.'
+  },
+  {
+    imgUrl: 'assets/products/saunaMachine.jpg',
+    caption: 'Sauna Machine.'
+  },
+  {
+    imgUrl: 'assets/products/elevators.jpg',
+    caption: 'Customized Elevetors.'
+  },
+  {
+    imgUrl: 'assets/products/cableCarGondola.jpg',
+    caption: 'Cable Car Gondola.'
+  },
+  {
+    imgUrl: 'assets/products/washingMachine.jpg',
+    caption: 'Washing Machine'
+  },
+  {
+    imgUrl: 'assets/products/woodFiredThreePassDryer.jpg',
+    caption: 'Wood Fired Three Pass Dryer.'
+  },
+  {
+    imgUrl: 'assets/products/trolleys.jpg',
+    caption: 'Trolleys'
+  },
+  {
+    imgUrl: 'assets/products/woodFiredVerticalTubeBoiler.jpg',
+    caption: 'Wood Fired Vertical Tube Boiler.'
+  }
+];
+
+//counter variable
+let i = 0;
+
+//code to change backgroundImage
+const changeBackgroundImageAndCaption = (imgUrl, caption) => {
+  carousel.style.backgroundImage = `url(${imgUrl})`;
+  captionText.innerText = caption;
+};
+
+//function to make code wait synchronously
+const wait = ms => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
+};
+
+//code to start slider
+const startSlider = async () => {
+  if (i == carouselData.length + 2) {
+    return;
+  }
+  for (i = 0; i < carouselData.length; i++) {
+    changeBackgroundImageAndCaption(
+      carouselData[i].imgUrl,
+      carouselData[i].caption
+    );
+    if (i == carouselData.length - 1) {
+      i = -1;
+    }
+    await wait(4000);
+  }
+};
+
+//forward image
+let k = 0;
+rightBtnImg.addEventListener('click', () => {
+  if (k == carouselData.length) {
+    k = 0;
+  }
+  i = carouselData.length + 2;
+  changeBackgroundImageAndCaption(
+    carouselData[k].imgUrl,
+    carouselData[k].caption
+  );
+  k++;
+});
+
+//backward image
+let j = carouselData.length - 1;
+backBtnImg.addEventListener('click', () => {
+  if (j < 0) {
+    j = carouselData.length - 1;
+  }
+  changeBackgroundImageAndCaption(
+    carouselData[j].imgUrl,
+    carouselData[j].caption
+  );
+  j--;
+});
+
+//replay
+replay.addEventListener('click', () => {
+  location.reload();
+});
+
+startSlider();
